bba040d427ff72edf8dfa21c99f7f5258df3b5ca
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 19:32:11 -0600

    Logica Completa!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// Contar cuántos dígitos tiene un número.
+Console.WriteLine("Ingrese un número");
+int num = int.Parse(Console.ReadLine());
 
+int contador = 0;
+
+ while (num != 0)
+{
+    num = num / 10;
+    contador++;
+}
+
+Console.WriteLine("La cantidad de digitos: "+contador);
